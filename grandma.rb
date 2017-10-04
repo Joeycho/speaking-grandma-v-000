1b334c62b2e4b8994a26a576468c69f8f394e337
Joey Cho <cjy12182000@naver.com>
2017-10-04 12:49:42 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,17 +2,17 @@
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif input ==nil
-    puts "HUH?! SPEAK UP, SONNY!"
-  else
-    puts"NO, NOT SINCE 1938!"
+    elsif input ==nil
+      puts "HUH?! SPEAK UP, SONNY!"
+      else
+        puts"NO, NOT SINCE 1938!"
 end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!

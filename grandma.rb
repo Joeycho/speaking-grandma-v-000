aeb35fd584311420480b45a5398ad83f8f3e8e4c
Joey Cho <cjy12182000@naver.com>
2017-10-04 12:47:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(input)
+  if input == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif input ==nil
+    puts "HUH?! SPEAK UP, SONNY!"
+  else
+    puts"NO, NOT SINCE 1938!"
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!

1c91ffd531a0528eb93e990c70ba0887970c2539
Joey Cho <cjy12182000@naver.com>
2017-10-04 12:53:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(input)
   if input == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-    elsif input == nil
+  elsif input != input.uppercase
       puts "HUH?! SPEAK UP, SONNY!"
       else
         puts"NO, NOT SINCE 1938!"
